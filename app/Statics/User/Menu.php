d1375a79ae7db52184d99c35ab43e01e9d3325a0
Rizki Tarang <frtarang@gmail.com>
2022-12-23 16:45:34 +0700

    [Updated]Make menu_id and permission_id standarization

--- app/Statics/User/Menu.php
+++ app/Statics/User/Menu.php
@@ -7,13 +7,13 @@ class Menu
     public static $DASHBOARD = 1000;
     
     public static $UTILITY = 8000;
-    public static $UTILITY_USER = 8001;
-    public static $UTILITY_MENU = 8002;
-    public static $UTILITY_ROLE = 8003;
-    public static $UTILITY_PERMISSION = 8004;
-    public static $UTILITY_SECURITY = 8005;
+    public static $UTILITY_USER = 8100;
+    public static $UTILITY_MENU = 8200;
+    public static $UTILITY_ROLE = 8300;
+    public static $UTILITY_PERMISSION = 8400;
+    public static $UTILITY_SECURITY = 8500;
 
     public static $USERS_ACTIVITY = 9000;
-    public static $USERS_ACTIVITY_LAST_SEEN = 9001;
-    public static $USERS_ACTIVITY_LOG_ACTIVITY = 9002;
+    public static $USERS_ACTIVITY_LAST_SEEN = 9100;
+    public static $USERS_ACTIVITY_LOG_ACTIVITY = 9200;
 }
--- app/Statics/User/Permission.php
+++ app/Statics/User/Permission.php
@@ -5,33 +5,33 @@ namespace App\Statics\User;
 class Permission
 {
     // permissions
-    public static $PERMISSION_LIST = 6000;
-    public static $PERMISSION_CREATE = 6001;
-    public static $PERMISSION_EDIT = 6002;
-    public static $PERMISSION_DELETE = 6003;
+    public static $PERMISSION_LIST = 6900;
+    public static $PERMISSION_CREATE = 6901;
+    public static $PERMISSION_EDIT = 6902;
+    public static $PERMISSION_DELETE = 6903;
     
     // roles
-    public static $ROLE_LIST = 7000;
-    public static $ROLE_CREATE = 7001;
-    public static $ROLE_EDIT = 7002;
-    public static $ROLE_DELETE = 7003;
+    public static $ROLE_LIST = 7900;
+    public static $ROLE_CREATE = 7901;
+    public static $ROLE_EDIT = 7902;
+    public static $ROLE_DELETE = 7903;
     
     // menus
-    public static $MENU_LIST = 8000;
-    public static $MENU_CREATE = 8001;
-    public static $MENU_EDIT = 8002;
-    public static $MENU_DELETE = 8003;
+    public static $MENU_LIST = 8900;
+    public static $MENU_CREATE = 8901;
+    public static $MENU_EDIT = 8902;
+    public static $MENU_DELETE = 8903;
     
     // users
-    public static $USER_LIST = 9000;
-    public static $USER_CREATE = 9001;
-    public static $USER_EDIT = 9002;
-    public static $USER_DELETE = 9003;
-    public static $USER_RESET_PASSWORD = 9004;
-    public static $USER_UNBLOCK = 9005;
-    public static $USER_REMOVE_IP = 9006;
-    public static $USERS_LAST_SEEN = 9007;
-    public static $USERS_LOG_ACTIVITY = 9008;
+    public static $USER_RESET_PASSWORD = 9001;
+    public static $USER_UNBLOCK = 9002;
+    public static $USER_REMOVE_IP = 9003;
+    public static $USERS_LAST_SEEN = 9004;
+    public static $USERS_LOG_ACTIVITY = 9005;
+    public static $USER_LIST = 9900;
+    public static $USER_CREATE = 9901;
+    public static $USER_EDIT = 9902;
+    public static $USER_DELETE = 9903;
     
     // security
     public static $SECURITY = 9999;
